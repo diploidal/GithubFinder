35a9222fd2b4d86017033878255ad0283685b484
Maksymilian Pryliński <diploidal@gmail.com>
2017-01-22 15:20:02 +0100

    jquery fetch info about user thats in search box

--- js/app.js
+++ js/app.js
@@ -11,7 +11,24 @@ $(document).ready(function(){
 			}
 
 		}).done(function(user){
-			console.log(user);
-		})
+			$('#profile').html(`
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">${user.name}</h3>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="thumbnail avatar" src="${user.avatar_url}">
+								<a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}"> View Profile</a>
+						</div>
+						<div class="col_md-9">
+
+						</div>
+					</div>
+				</div>
+			</div>
+			`);
+		});
 	});
 });

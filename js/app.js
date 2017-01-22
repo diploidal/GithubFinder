7fcc9f9cc77ee61c8ebf0fb5854b754e20f62f58
Maksymilian Pryliński <diploidal@gmail.com>
2017-01-22 14:51:53 +0100

    Created a request to github everytime you write anything in search box

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 	$('#searchUser').on('keyup', function(e) {
 		let userName = e.target.value;
-		
+
+		// making requety ti github
+		$.ajax({
+			url:'https://api.github.com/users/' + userName,
+			data:{
+				client_id:'1df8bb3f148d51fc631e',
+				client_secret:'b5c867d1368528e052ab91ba688fc43195d015e9'
+			}
+
+		}).done(function(user){
+			console.log(user);
+		})
 	});
 });
